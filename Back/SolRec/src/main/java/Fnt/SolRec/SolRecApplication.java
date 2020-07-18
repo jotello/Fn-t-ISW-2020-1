bbbb82eb6b016f046722047bd38dd13dd75cbe46
Martin Miranda <martin.miranda.14@sansano.usm.cl>
2020-07-17 22:00:41 -0400

    Primera subida

---
+++ Back/SolRec/src/main/java/Fnt/SolRec/SolRecApplication.java
@@ -0,0 +1,13 @@
+package Fnt.SolRec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolRecApplication.class, args);
+	}
+
+}
---
+++ Back/SolRec/src/test/java/Fnt/SolRec/SolRecApplicationTests.java
@@ -0,0 +1,13 @@
+package Fnt.SolRec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

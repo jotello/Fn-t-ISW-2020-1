856a9b36d29bda572a61f36984de58109732a621
martinmiranda14 <martin.miranda.14@sansano.usm.cl>
2020-07-20 20:25:37 -0400

    Agregada 4

--- Back/SolRec/src/main/java/Fnt/SolRec/Controller/SolicitudController.java
+++ Back/SolRec/src/main/java/Fnt/SolRec/Controller/SolicitudController.java
@@ -6,10 +6,15 @@ import java.util.Optional;
 
 import org.hibernate.loader.custom.Return;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import Fnt.SolRec.Model.Solicitud;
 import Fnt.SolRec.Service.SolicitudService;
@@ -30,6 +35,14 @@ public class SolicitudController {
         return solicitudService.listAll();
 
     }
+    /**
+     * 
+     * @param id
+     * 
+     * Obtiene la solicitud cuya id sea id
+     * 
+     * @return
+     */
     @GetMapping("/{id}")
     public Optional<Solicitud> getSolicitudbyId(@PathVariable("id") Long id){
         return solicitudService.getbyId(id);
@@ -41,6 +54,19 @@ public class SolicitudController {
     public List<Solicitud> getPendientes() {
         List<String> list = Arrays.asList("Solicitado");
         return (List<Solicitud>) solicitudService.getSolicitudByEstado(list);
+    }
+    /**
+     * 
+     * @param solicitud
+     * 
+     * Añade las solicitudes a la DB
+     * 
+     * @return Solicitud y HttpStatus
+     */
+    @PostMapping("")
+    public ResponseEntity<Solicitud> addSolicitud(@RequestBody Solicitud solicitud){
+        Solicitud sol= solicitudService.saveOrUpdateSolicitud(solicitud);
+        return new ResponseEntity<Solicitud>(sol, HttpStatus.CREATED);
     }
 
 }

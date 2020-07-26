f72557b67386c7ba8474a68d3646f1ac0ce2058b
rdiaz <rodrigo.diazde@sansano.usm.cl>
2020-07-25 21:24:29 -0400

    Se añade DELETE en pacientes

--- Back/SolRec/src/main/java/Fnt/SolRec/Controller/PacienteController.java
+++ Back/SolRec/src/main/java/Fnt/SolRec/Controller/PacienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,29 +18,29 @@ import Fnt.SolRec.Service.PacienteService;
 @RestController
 @RequestMapping("paciente/")
 public class PacienteController {
-	
+
 	@Autowired
 	private PacienteService pacienteService;
-	
+
 	/*
 	 * Obtener lista de pacientes
-	 *@return List Pacientes */	
+	 *@return List Pacientes */
 	@GetMapping("")
 	public Iterable<Paciente> getPacientes(){
 		return pacienteService.listAll();
 	}
 	/*
 	 * Crea Paciente
-	 * 
+	 *
 	 * @return Paciente HttpStatus.Created
 	 * */
 	@PostMapping("")
 	public ResponseEntity<Paciente> addPaciente(Paciente paciente){
 		Paciente pas = pacienteService.saveOrUpdatePaciente(paciente);
 		return new ResponseEntity<Paciente>(pas, HttpStatus.CREATED);
-		
+
 	}
-	
+
 	/*
 	 *Se obtiene paciente por id
 	 *
@@ -49,13 +50,17 @@ public class PacienteController {
 	public Optional<Paciente> getPacientebyId(@PathVariable("id") long id){
 		return pacienteService.getbyId(id);
 	}
-	
+
 	/*
 	 * Aca va Modificar
 	 * */
-	
-	/*
-	 * Aca va eliminar 
-	 * */
 
+	/*
+	 * Aca va eliminar
+	 * */
+	@DeleteMapping("/{id}")
+	public @ResponseBody ResponseEntity<String>  deletePaciente(@PathVariable("id") long id){
+		PacienteService.delete(id);
+		return new ResponseEntity<String>("Paciente eliminado", HttpStatus.OK);
+	}
 }
